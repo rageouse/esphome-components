a2491bf72b597addc575cda8019fe9ec190b87b6
John McManigle <john@oxfordechoes.com>
2023-03-21 17:21:32 -0400

    First try at RGB only

---
+++ components/qwiic_twist/qwiic_twist.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "esphome.h"
+#include "esphome/components/rgb/rgb_light_output.h"
+
+namespace esphome {
+namespace qwiic_twist {
+
+class QwiicTwist : public rgb::RGBLightOutput {
+
+  public:
+    void set_red(output::FloatOutput *red)     override { ; }
+    void set_green(output::FloatOutput *green) override { ; }
+    void set_blue(output::FloatOutput *blue)   override { ; }
+    
+    void write_state(light::LightState *state) override {
+      float red, green, blue;
+      state->current_values_as_rgb(&red, &green, &blue, false);
+
+      Wire.beginTransmission(i2c_address_);
+      Wire.write(0x0D); // Red
+      Wire.write(char(red*255));
+      Wire.endTransmission();
+      
+      Wire.beginTransmission(i2c_address_);
+      Wire.write(0x0E); // Green
+      Wire.write(char(green*255));
+      Wire.endTransmission();
+      
+      Wire.beginTransmission(i2c_address_);
+      Wire.write(0x0F); // Blue
+      Wire.write(char(blue*255));
+      Wire.endTransmission();
+    }
+  
+  protected:
+    char i2c_address_ = 0x3F;
+
+};
+
+} // namespace qwiic_twist
+} // namespace esphome
