55bc579d1a066fa2297f6ea55ddcb36122c15ae0
John McManigle <john@oxfordechoes.com>
2023-03-21 17:33:40 -0400

    Apparently there's python involved too

---
+++ components/qwiic_twist/qwiic_twist.py
@@ -0,0 +1,19 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import light, output
+from esphome.const import CONF_BLUE, CONF_GREEN, CONF_RED, CONF_OUTPUT_ID
+
+qwiic_twist_ns = cg.esphome_ns.namespace("qwiic_twist")
+QwiicTwist = qwiic_twist_ns.class_("QwiicTwist", light.LightOutput)
+
+CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
+    {
+        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(RGBLightOutput),
+    }
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    await light.register_light(var, config)
+
