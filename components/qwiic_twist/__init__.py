6cedafeb0d5cf7b002bd2d04b7a08466bbe669d7
John McManigle <john@oxfordechoes.com>
2023-03-21 22:51:39 -0400

    Come sail away?

--- components/qwiic_twist/__init__.py
+++ components/qwiic_twist/__init__.py
@@ -1,18 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import light
+from esphome.components import light, i2c
 from esphome.const import CONF_OUTPUT_ID
 
 qwiic_twist_ns = cg.esphome_ns.namespace("qwiic_twist")
 QwiicTwist = qwiic_twist_ns.class_("QwiicTwist", light.LightOutput)
 
+DEPENDENCIES = ["light", "i2c"]
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(QwiicTwist),
-    }
-).extend(light.RGB_LIGHT_SCHEMA).extend(cv.COMPONENT_SCHEMA)
-
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(QwiicTwist),
+        }
+    )
+    .extend(light.RGB_LIGHT_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
