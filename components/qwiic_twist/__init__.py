a5bbc86c88bc147334e3b9ecc5c41cfd325d3c2e
John McManigle <john@oxfordechoes.com>
2023-03-26 12:13:29 -0400

    More play with python

--- components/qwiic_twist/__init__.py
+++ components/qwiic_twist/__init__.py
@@ -5,8 +5,13 @@ from esphome.const import CONF_OUTPUT_ID
 
 qwiic_twist_ns = cg.esphome_ns.namespace("qwiic_twist")
 QwiicTwist        = qwiic_twist_ns.class_("QwiicTwist", cg.Component)
-QwiicTwistRGB     = qwiic_twist_ns.class_("QwiicTwistRGB", light.LightOutput, cg.Component)
-QwiicTwistEncoder = qwiic_twist_ns.class_("QwiicTwistEncoder", sensor.Sensor, cg.PollingComponent)
+
+QwiicTwistRGB     = QwiicTwist.twist_rgb_
+QwiicTwistEncoder = QwiicTwist.twist_encoder_
+
+
+# QwiicTwistRGB     = qwiic_twist_ns.class_("QwiicTwistRGB", light.LightOutput, cg.Component)
+# QwiicTwistEncoder = qwiic_twist_ns.class_("QwiicTwistEncoder", sensor.Sensor, cg.PollingComponent)
 
 DEPENDENCIES = ["light", "sensor", "i2c"]
 AUTO_LOAD = ["light", "sensor"]
@@ -14,7 +19,9 @@ AUTO_LOAD = ["light", "sensor"]
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(QwiicTwist),
+            cv.GenerateID(CONF_COMPONENT_ID): cv.declare_id(QwiicTwist),
+            cv.GenerateID(CONF_LIGHT_ID):     cv.declare_id(QwiicTwistRGB),
+            cv.GenerateID(CONF_SENSOR_ID):    cv.declare_id(QwiicTwistEncoder),
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -23,8 +30,12 @@ CONFIG_SCHEMA = (
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    var = cg.new_Pvariable(config[CONF_COMPONENT_ID])
     await cg.register_component(var, config)
+    
+    var = cg.new_Pvariable(config[CONF_LIGHT_ID])
     await light.register_light(var, config)
+
+    var = cg.new_Pvariable(config[CONF_SENSOR_ID])
     await sensor.register_sensor(var, config)
 
