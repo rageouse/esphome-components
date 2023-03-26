76db77430444d38ea560d9451aca15b79b4e32b0
John McManigle <john@oxfordechoes.com>
2023-03-26 19:52:25 -0400

    Try limiters

--- components/qwiic_twist/light/qwiic_twist_rgb.cpp
+++ components/qwiic_twist/light/qwiic_twist_rgb.cpp
@@ -16,14 +16,30 @@ light::LightTraits QwiicTwistRGB::get_traits() {
   return traits;
 }
 
+uint8_t QwiicTwistRGB::red_float_to_uint8( float v ) {
+    if( v < 0.01 ) return 0;
+    v = v * (this->red_max_ - this->red_min_) + this->red_min_;
+    return static_cast<uint8_t>(v * 255);
+}
+uint8_t QwiicTwistRGB::green_float_to_uint8( float v ) {
+    if( v < 0.01 ) return 0;
+    v = v * (this->green_max_ - this->green_min_) + this->green_min_;
+    return static_cast<uint8_t>(v * 255);
+}
+uint8_t QwiicTwistRGB::blue_float_to_uint8( float v ) {
+    if( v < 0.01 ) return 0;
+    v = v * (this->blue_max_ - this->blue_min_) + this->blue_min_;
+    return static_cast<uint8_t>(v * 255);
+}
+
 void QwiicTwistRGB::write_state(light::LightState *state) {
   float red, green, blue;
   state->current_values_as_rgb(&red, &green, &blue, false);
   
   uint8_t buf[] = { 0x0D, // 0x0D = Red; 0x0E = Green; 0x0F = Blue; register auto-increments
-                    red * 255,
-                    green * 255,
-                    blue * 255
+                    this->red_float_to_uint8(red),
+                    this->green_float_to_uint8(green),
+                    this->blue_float_to_uint8(blue)
                   };
 
   if( this->parent_->write(buf, 4) != i2c::ERROR_OK )
--- components/qwiic_twist/light/qwiic_twist_rgb.h
+++ components/qwiic_twist/light/qwiic_twist_rgb.h
@@ -24,9 +24,23 @@ class QwiicTwistRGB : public light::LightOutput, public Component {
     void set_parent(QwiicTwist *parent) {
       this->parent_ = parent;
     }
+    
+    void set_red_min  ( float v ) { this->red_min_   = v; }
+    void set_red_max  ( float v ) { this->red_max_   = v; }
+    void set_green_min( float v ) { this->green_min_ = v; }
+    void set_green_max( float v ) { this->green_max_ = v; }
+    void set_blue_min ( float v ) { this->blue_min_  = v; }
+    void set_blue_max ( float v ) { this->blue_max_  = v; }
 
   protected:
     QwiicTwist *parent_ { nullptr };
+    
+    float red_min_   { 0.14 };
+    float red_max_   { 1.0  };
+    float green_min_ { 0.14 };
+    float green_max_ { 1.0  };
+    float blue_min_  { 0.14 };
+    float blue_max_  { 1.0  };
 
 };
 
