9df425d8f7ca0a2d5b9055ca9839c75c9bcdde71
John McManigle <john@oxfordechoes.com>
2023-07-31 20:41:34 -0400

    C++

--- components/qwiic_joystick/qwiic_joystick.cpp
+++ components/qwiic_joystick/qwiic_joystick.cpp
@@ -14,10 +14,10 @@ static const char *const TAG = "qwiic_joystick";
 void QwiicJoystick::setup() {
   ESP_LOGCONFIG(TAG, "Setting up Qwiic Joystick...");
   
-  uint16_t version;
+  uint16_t version[];
   
-  if( this->read_bytes_16(0x01, &version, 1) )
-    ESP_LOGCONFIG(TAG, "- Version %d.", version);
+  if( this->read_bytes(0x01, version, 2) )
+    ESP_LOGCONFIG(TAG, "- Version %d.%d", version[0], version[1]);
   else
     ESP_LOGCONFIG(TAG, "- Error: could not read version number!");
 
@@ -41,8 +41,8 @@ void QwiicJoystick::update() {
   uint8_t buf[6];
   this->read_bytes(0x03, buf, 6);
   
-  uint16_t x = ((buf[0] << 8) | buf[1]) >> 6;
-  uint16_t y = ((buf[2] << 8) | buf[3]) >> 6;
+  uint16_t x = ((static_cast<uint16_t>(buf[0]) << 8) | static_cast<uint16_t>(buf[1])) >> 6;
+  uint16_t y = ((static_cast<uint16_t>(buf[2]) << 8) | static_cast<uint16_t>(buf[3])) >> 6;
   
   int16_t x_c = static_cast<int16_t>(x) - this->center_x_;
   int16_t y_c = static_cast<int16_t>(y) - this->center_y_;
@@ -51,7 +51,8 @@ void QwiicJoystick::update() {
     if( buf[5] )
         this->button_sensor_->publish_state(true);
     if( buf[5] || buf[4] != this->old_button_pressed_ )
-        this->button_sensor_->publish_state(static_cast<bool>(buf[4]));
+        this->button_sensor_->publish_state(! static_cast<bool>(buf[4]));
+    this->write_byte(0x08, 0x00);
   }  
   
   for(uint8_t i = 0; i < 6; i++) {
