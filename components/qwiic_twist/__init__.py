d1a9f6c270db5af66714e8bc3477a3190a7d2e9e
John McManigle <john@oxfordechoes.com>
2023-08-03 13:53:45 -0400

    Ha ha ha

--- components/qwiic_twist/__init__.py
+++ components/qwiic_twist/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import light, sensor, i2c
+from esphome.components import light, sensor, binary_sensor, i2c
 from esphome import automation
 from esphome.const import (
     CONF_ID,
